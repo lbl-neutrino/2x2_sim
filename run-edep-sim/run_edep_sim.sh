a0ef25d1d67a0e97ebe232aece5d4e7d7c5aff8c
Tiago Tavares Alves <tta20@login16.chn.perlmutter.nersc.gov>
2024-11-15 08:23:29 -0800

    made changes to make it work using the environments variables like all the other ennv_vars in 2x2_sim

--- run-edep-sim/run_edep_sim.sh
+++ run-edep-sim/run_edep_sim.sh
@@ -5,8 +5,14 @@ source ../util/init.inc.sh
 
 
 
-if [ "$ARCUBE_BEAM_TYPE" = "particle_gun" ]; then
-    nEvents=$ARCUBE_EXPOSURE
+if [ $(echo "$ARCUBE_BEAM_TYPE") = "particle_gun" ]; 
+    then nEvents=$ARCUBE_EXPOSURE
+    rm -f "macros/particle-gun-modified.mac"
+    sed -e "s/@ARCUBE_PARTICLE_TYPE@/$ARCUBE_PARTICLE_TYPE/g" \
+        -e "s/@ARCUBE_ENERGY_MINIMUM@/$ARCUBE_ENERGY_MINIMUM/g" \
+        -e "s/@ARCUBE_ENERGY_MAXIMUM@/$ARCUBE_ENERGY_MAXIMUM/g" \
+        $ARCUBE_EDEP_MAC > macros/modified-temp-file.mac
+    export ARCUBE_EDEP_MAC='macros/modified-temp-file.mac'
 else
     genieOutPrefix=${ARCUBE_OUTDIR_BASE}/run-genie/${ARCUBE_GENIE_NAME}/GTRAC/$subDir/${ARCUBE_GENIE_NAME}.$globalIdx
     genieFile="$genieOutPrefix".GTRAC.root
@@ -14,13 +20,13 @@ else
     auto t = (TTree*) _file0->Get("gRooTracker");
     std::cout << t->GetEntries() << std::endl;'
     nEvents=$(echo "$rootCode" | root -l -b "$genieFile" | tail -1)
+
+    edepCode="/generator/kinematics/rooTracker/input $genieFile
+    /edep/runId $runNo"
 fi
 
 edepRootFile=$tmpOutDir/${outName}.EDEPSIM.root
 rm -f "$edepRootFile"
-
-edepCode="/generator/kinematics/rooTracker/input $genieFile
-/edep/runId $runNo"
 
 # The geometry file is given relative to the root of 2x2_sim
 export ARCUBE_GEOM_EDEP=$baseDir/${ARCUBE_GEOM_EDEP:-$ARCUBE_GEOM}
@@ -30,3 +36,7 @@ run edep-sim -C -g "$ARCUBE_GEOM_EDEP" -o "$edepRootFile" -e "$nEvents" \
 
 mkdir -p "$outDir/EDEPSIM/$subDir"
 mv "$edepRootFile" "$outDir/EDEPSIM/$subDir"
+
+if [ -f "macros/particle-gun-modified.mac" ]; then
+    rm -f "macros/particle-gun-modified.mac"
+fi
