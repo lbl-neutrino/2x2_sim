29d158c8836285b2d4289a8d19ebc358c8413266
alexbooth92 <a.c.booth92@gmail.com>
2025-01-25 03:08:13 -0800

    Make edepsim files a compulsory argument and the two reconstructions non-compulsory now that we have too. Changes made to gen_cafmaker_cfg to ensure that one or both of the reconstructions is included.

--- run-cafmaker/run_cafmaker.sh
+++ run-cafmaker/run_cafmaker.sh
@@ -13,11 +13,10 @@ outFile=${tmpOutDir}/${outName}.CAF.root
 flatOutFile=${tmpOutDir}/${outName}.CAF.flat.root
 cfgFile=$(mktemp --suffix .cfg)
 
-# Compulsory arguments.
+# Compulsory arguments regardless of use case.
 args_gen_cafmaker_cfg=( \
     --base-dir "$ARCUBE_OUTDIR_BASE" \
-    --spine-name "$ARCUBE_SPINE_NAME" \
-    --pandora-name "$ARCUBE_PANDORA_NAME" \
+    --edepsim-name "$ARCUBE_SPILL_NAME" \
     --caf-path "$outFile" \
     --cfg-file "$cfgFile" \
     --file-id "$ARCUBE_INDEX" \
@@ -25,8 +24,9 @@ args_gen_cafmaker_cfg=( \
 
 [ -n "${ARCUBE_GHEP_NU_NAME}" ] && args_gen_cafmaker_cfg+=( --ghep-nu-name "$ARCUBE_GHEP_NU_NAME" )
 [ -n "${ARCUBE_GHEP_ROCK_NAME}" ] && args_gen_cafmaker_cfg+=( --ghep-rock-name "$ARCUBE_GHEP_ROCK_NAME" )
-[ -n "${ARCUBE_SPILL_NAME}" ] && args_gen_cafmaker_cfg+=( --edepsim-name "$ARCUBE_SPILL_NAME" )
 [ -n "${ARCUBE_MINERVA_NAME}" ] && args_gen_cafmaker_cfg+=( --minerva-name "$ARCUBE_MINERVA_NAME" )
+[ -n "${ARCUBE_SPINE_NAME}" ] && args_gen_cafmaker_cfg+=( --spine-name "$ARCUBE_SPINE_NAME" )
+[ -n "${ARCUBE_PANDORA_NAME}" ] && args_gen_cafmaker_cfg+=( --pandora-name "$ARCUBE_PANDORA_NAME" )
 [ -n "${ARCUBE_TMSRECO_NAME}" ] && args_gen_cafmaker_cfg+=( --tmsreco-name "$ARCUBE_TMSRECO_NAME" )
 [ -n "${ARCUBE_HADD_FACTOR}" ] && args_gen_cafmaker_cfg+=( --hadd-factor "$ARCUBE_HADD_FACTOR" )
 [ -n "${ARCUBE_EXTRA_LINES}" ] && args_gen_cafmaker_cfg+=( --extra-lines "$ARCUBE_EXTRA_LINES" )
